b959e50d9b53acb32136ae96052ff121cfc61161
Xianfeng Zhuge <xianfeng19971117@gmail.com>
2024-04-27 23:41:30 +1000

    Update chat_gpt_bot.py

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -148,10 +148,12 @@ class ChatGPTBot(Bot, OpenAIImage):
                             last_newline = response_str.rfind('\n')
                             if last_newline != -1:
                                 logger.info("yield response_str: " + response_str[:last_newline])
+                                logger.info("response_prev: " + response_prev)
                                 yield response_str[:last_newline]
                                 response_prev = response_str[:last_newline]
                             else:
-                                logger.info("yield response_str: " + response_str)
+                                logger.info("yield response_str_no_line: " + response_str)
+                                logger.info("response_prev_no_line: " + response_prev)
                                 yield response_str[len(response_prev):]
                                 response_prev = response_str
 
