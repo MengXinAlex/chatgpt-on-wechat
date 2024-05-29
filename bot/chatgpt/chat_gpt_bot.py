2b07ec4334af234d0b232f88119a86b9909f9c76
Xianfeng Zhuge <xianfeng19971117@gmail.com>
2024-05-29 10:08:01 +1000

    Update chat_gpt_bot.py

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -63,10 +63,9 @@ class ChatGPTBot(Bot, OpenAIImage):
                 reply = Reply(ReplyType.INFO, "配置已更新")
             elif query == "我在测试消息跳转":
                 load_config()
-                reply = Reply(ReplyType.INFO, "<a href=\"weixin://bizmsgmenu?msgmenucontent=点击自动发送测试成功&msgmenuid=1"
-                                              "\">请点击文字</a>")
+                reply = "<a href=\"weixin://bizmsgmenu?msgmenucontent=点击自动发送测试成功&msgmenuid=1\">请点击文字</a>"
             if reply:
-                return reply
+                yield reply
             session = self.sessions.session_query(query, session_id)
             logger.debug("[CHATGPT] session query={}".format(session.messages))
 
